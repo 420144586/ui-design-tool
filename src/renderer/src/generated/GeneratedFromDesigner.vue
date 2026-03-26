9020f00f1ee2dbd2cfa477ab93c65cddcd85339c
accelerato <420144586@qq.com>
2026-03-26 23:59:25 +0800

    添加了若干功能

--- src/renderer/src/generated/GeneratedFromDesigner.vue
+++ src/renderer/src/generated/GeneratedFromDesigner.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="el-1774523698822-b770d1">
-      <div class="el-1774523726776-71325e"></div>
+    <div class="el-1774523251182-f9b037">
+      <div class="el-1774523370017-f38503"></div>
     </div>
 </template>
 
@@ -9,32 +9,36 @@
 </script>
 
 <style scoped>
-.el-1774523698822-b770d1 {
+.el-1774523251182-f9b037 {
   grid-column: 1 / span 96;
   grid-row: 1 / span 42;
   width: 960px;
   height: 420px;
-  background: #fff;
+  background: linear-gradient(180deg, #FFFFFF, #F2F8FF,  #F7FBFF );
   opacity: 0.9;
   position: relative;
   display: grid;
   place-items: center;
   grid-template-columns: repeat(96, 10px);
   grid-template-rows: repeat(42, 10px);
+  border-top: 2px solid #A4B7CC;
+  border-right: 2px solid #A4B7CC;
+  border-bottom: 2px solid #A4B7CC;
+  border-left: 2px solid #A4B7CC;
   box-sizing: border-box;
 }
-.el-1774523726776-71325e {
+.el-1774523370017-f38503 {
   grid-column: 1 / span 96;
   grid-row: 37 / span 6;
-  width: 100%;
+  width: 960px;
   height: 60px;
-  background: #ff8f3e;
+  background: transparent;
   opacity: 0.9;
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-top: 1px solid #000;
+  border-top: 2px solid #A4B7CC;
   border-right: none;
   border-bottom: none;
   border-left: none;
